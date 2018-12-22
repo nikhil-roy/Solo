e1d103f0c15535cdafcba49b4454cc8b209635c4
nikhil-roy <nikhilroy.net@gmail.com>
2018-12-23 00:40:34 +0530

    refactoring code and removing warnings

--- app/src/main/java/com/example/nikhil/solo/Canvas.java
+++ app/src/main/java/com/example/nikhil/solo/Canvas.java
@@ -3,8 +3,6 @@ package com.example.nikhil.solo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -21,9 +19,8 @@ public class Canvas extends AppCompatActivity {
         setData(data);
     }
     public void setData(String data){
-        JSONObject jsonObject = null;
         try {
-            jsonObject = new JSONObject(data);
+            JSONObject jsonObject = new JSONObject(data);
             TextView textView = findViewById(R.id.textView);
             textView.setText((String) jsonObject.get("data"));
         } catch (JSONException e) {
--- app/src/main/java/com/example/nikhil/solo/MainActivity.java
+++ app/src/main/java/com/example/nikhil/solo/MainActivity.java
@@ -1,45 +1,14 @@
 package com.example.nikhil.solo;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
 public class MainActivity extends AppCompatActivity {
-
-    OkHttpClient client = new OkHttpClient();
-    public String postUrl = "http://192.168.43.173:3000/login";
-    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    NetworkUtils networkUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,48 +26,8 @@ public class MainActivity extends AppCompatActivity {
                 "    \"password\": \"" + password + "\"\n" +
                 "}";
 
-        try {
-            postRequest(postUrl,postBody);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    void postRequest(String postUrl,String postBody) throws IOException {
-
-        OkHttpClient client = new OkHttpClient();
-
-        RequestBody body = RequestBody.create(JSON, postBody);
-
-        Request request = new Request.Builder()
-                .url(postUrl)
-                .post(body)
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                call.cancel();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                final String data = response.body().string();
-                Log.d("TAG",data);
-                try {
-                    JSONObject jsonObject = new JSONObject(data);
-                    if(jsonObject.has("status"))
-//                        Toast.makeText(getApplicationContext(),"Login Failed!",Toast.LENGTH_LONG).show();
-                        openActivity("Login Failed");
-                    else
-                        openActivity(data);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
+        String data = networkUtils.postRequest(postBody);
+        openActivity(data);
     }
 
 
